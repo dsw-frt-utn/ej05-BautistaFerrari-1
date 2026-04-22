3134b28b56e2dd417229066f7ea2f7095e1ed117
Bautista Ferrari Osado <BautistaFerrariOsado@alu.frt.utn.edu.ar>
2026-04-22 16:51:51 -0300

    dswej05

--- Dsw2026Ej5/Domain/VehiculoElectrico.cs
+++ Dsw2026Ej5/Domain/VehiculoElectrico.cs
@@ -22,10 +22,11 @@ public class VehiculoElectrico : Vehiculo
     public override double CalcularConsumo(double kilometros)
     {
         double total = kwhBase;
-        if (GetCapacidadCarga() >= 1200) 
+        if (GetCapacidadCarga() >= 1200)
         {
             total = total * 1.15;
         }
+
         return total;
     }
 }
